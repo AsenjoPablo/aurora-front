80684ca3b2150047988eba1e757445ce3c36cfb7
José Carlos Rodríguez Palacín <carlos.rodriguez.palacin@gmail.com>
2022-01-06 14:45:46 +0100

    add route repository

--- src/@aurora/routing/route-repository.service.ts
+++ src/@aurora/routing/route-repository.service.ts
@@ -1,7 +1,6 @@
 
 import { Inject, Injectable } from '@angular/core';
-import { API_DOMAIN, RouteDefinition , ROUTE_DEFINITIONS } from './routing.types';
-import { environment } from 'environments/environment';
+import { RouteDefinition , ROUTE_DEFINITIONS } from './routing.types';
 
 @Injectable({
     providedIn: 'root'
@@ -10,7 +9,6 @@ export class RouteRepositoryService
 {
     constructor(
         @Inject(ROUTE_DEFINITIONS) private routeRepository: RouteDefinition[],
-        @Inject(API_DOMAIN) private apiDomain: string,
     ) { }
 
     route(routeName: string, params?: {[key: string]: string}): string
@@ -22,21 +20,11 @@ export class RouteRepositoryService
                 useValue: routeRepository,
             }`);
 
-        if (!this.apiDomain) throw new Error(`
-            You must define API_DOMAIN tag provider, in your app.module with your end point domain, for example:
-            {
-                provide: API_DOMAIN,
-                useValue: 'https://mydomain.com',
-            }`);
-
         const routeMatch  = this.routeRepository.find(route => route.name === routeName) as RouteDefinition;
 
         if (!routeMatch) throw new Error(`The routeName ${routeName} not exist in your route repository`);
 
-        const interpolatedRoute = this.interpolate(routeMatch.route, params);
-
-        if (routeMatch?.internal) return interpolatedRoute;
-        return (routeMatch.mock ? environment.mock.rest : environment.api.rest) + '/' + interpolatedRoute;
+        return this.interpolate(routeMatch.route, params);
     }
 
     private interpolate(route: string, params?: {[key: string]: string}): string
--- src/@aurora/routing/routing.types.ts
+++ src/@aurora/routing/routing.types.ts
@@ -1,12 +1,9 @@
 import { InjectionToken } from '@angular/core';
 
-export const API_DOMAIN = new InjectionToken<string>('api.domain');
 export const ROUTE_DEFINITIONS = new InjectionToken<RouteDefinition[]>('route.definitions');
 
 export interface RouteDefinition
 {
     name     : string;
     route    : string;
-    mock?    : boolean;
-    internal?: boolean;
 }
