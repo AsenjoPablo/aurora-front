3a847bf76dc06dbe166108cb230e231f770a8933
José Carlos Rodríguez Palacín <carlos.rodriguez.palacin@gmail.com>
2022-01-07 23:11:54 +0100

    navigation

--- src/app/mock-api/common/navigation/data.ts
+++ src/app/mock-api/common/navigation/data.ts
@@ -3,11 +3,63 @@ import { FuseNavigationItem } from '@fuse/components/navigation';
 
 export const defaultNavigation: FuseNavigationItem[] = [
     {
-        id   : 'example',
-        title: 'Example',
-        type : 'basic',
-        icon : 'heroicons_outline:chart-pie',
-        link : '/example'
+        id      : 'apps',
+        title   : 'Applications',
+        subtitle: 'Custom made application designs',
+        type    : 'group',
+        icon    : 'heroicons_outline:home',
+        children: [
+            {
+                id      : 'kitchen-sink',
+                title   : 'KitchenSink',
+                type    : 'collapsable',
+                icon    : 'heroicons_outline:adjustments',
+                children: [
+                    {
+                        id   : 'material',
+                        title: 'Material Components',
+                        type : 'group',
+                        children: [
+                            {
+                                id   : 'form',
+                                title: 'Form Material',
+                                type : 'basic',
+                                icon : 'heroicons_outline:clipboard-list',
+                                link : '/kitchen-sink/form-material',
+                            },
+                        ]
+                    },
+                    {
+                        id   : 'ignite',
+                        title: 'Ignite Components',
+                        type : 'group',
+                        children: [
+                            {
+                                id   : 'form',
+                                title: 'Form Ignite',
+                                type : 'basic',
+                                icon : 'heroicons_outline:clipboard-list',
+                                link : '/kitchen-sink/form-ignite',
+                            },
+                            {
+                                id   : 'grid',
+                                title: 'Grid Ignite',
+                                type : 'basic',
+                                icon : 'heroicons_outline:chart-pie',
+                                link : '/kitchen-sink/grid-ignite',
+                            },
+                        ]
+                    }
+                ]
+            },
+            {
+                id   : 'example',
+                title: 'Example',
+                type : 'basic',
+                icon : 'heroicons_outline:chart-pie',
+                link : '/example',
+            },
+        ]
     }
 ];
 export const compactNavigation: FuseNavigationItem[] = [
