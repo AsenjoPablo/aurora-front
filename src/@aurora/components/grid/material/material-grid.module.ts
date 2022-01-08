cfeaf1acd8777fe86f0fe5475c12ad304d62ce44
José Carlos Rodríguez Palacín <carlos.rodriguez.palacin@gmail.com>
2022-01-08 16:42:38 +0100

    wip

--- src/@aurora/components/grid/material/material-grid.module.ts
+++ src/@aurora/components/grid/material/material-grid.module.ts
@@ -1,12 +1,14 @@
 import { CommonModule } from '@angular/common';
 import { NgModule } from '@angular/core';
 import { MatTableModule } from '@angular/material/table';
+import { PipesModule } from '@aurora/pipes/pipes.module';
 import { MaterialGridComponent } from './material-grid.component';
 
 @NgModule({
     imports: [
         CommonModule,
         MatTableModule,
+        PipesModule,
     ],
     declarations: [
         MaterialGridComponent,
