bb9b914f906a5994ef9b4bdbd8d9b85ef4ae1dd9
José Carlos Rodríguez Palacín <carlos.rodriguez.palacin@gmail.com>
2022-01-11 19:24:54 +0100

    wip

--- src/@aurora/foundations/view-list.component.ts
+++ src/@aurora/foundations/view-list.component.ts
@@ -1,34 +1,13 @@
-import { Directive, Injector, OnInit } from '@angular/core';
-import { GridData, log } from '@aurora';
-import { first, map, Observable } from 'rxjs';
+import { Directive, Injector } from '@angular/core';
 import { ViewBaseComponent } from './view-base.component';
 
 @Directive()
 export class ViewListComponent extends ViewBaseComponent
 {
-    
-
     constructor(
         protected injector: Injector,
     )
     {
         super(injector);
     }
-
-    ngOnInit(): void
-    {
-        // this.getGridData();
-    }
-
-    /* async getGridData()
-    {
-        this.gridData$ = this.graphqlService
-            .client()
-            .watchQuery<{ corePagination: GridData; }>({
-                query: this.graphQLStatements.queryPagination,
-                variables: {}
-            })
-            .valueChanges
-            .pipe(map<{ data: { corePagination: GridData; }}, GridData>(result => result.data.corePagination));
-    } */
 }
